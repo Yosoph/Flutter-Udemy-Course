faadfc06fe3cde6bd40d0ecceebb966441843221
Yosoph <yosophmail@gmail.com>
2022-09-21 15:44:16 +0200

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:projects/modules/home/home_screen.dart';
 import 'package:projects/modules/login/login_screen.dart';
 import 'package:projects/modules/messenger/messenger_screen.dart';
 import 'package:projects/modules/users/users_screen.dart';
+import 'layout/home_layout.dart';
 import 'modules/bmi_result/bmi_result_screen.dart';
 import 'modules/bmi/bmi_screen.dart';
 import 'modules/counter/counter_screen.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: HomeLayout(),
     );
   }
 }
